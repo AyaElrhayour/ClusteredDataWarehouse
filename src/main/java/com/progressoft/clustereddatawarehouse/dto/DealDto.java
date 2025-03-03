d5d4700223e66b484d1ef525a69df240295c58ee
AyaElrhayour <129304439+AyaElrhayour@users.noreply.github.com>
2025-03-03 07:17:12 +0000

    added some more validations

--- src/main/java/com/progressoft/clustereddatawarehouse/dto/DealDto.java
+++ src/main/java/com/progressoft/clustereddatawarehouse/dto/DealDto.java
@@ -18,14 +18,17 @@ public class DealDto {
 
     @NotNull(message = "Id must not be null")
     @NotEmpty(message = "Id must not be empty")
+    @Size(max = 255, min = 3, message = "Id should be between 3 and 255 characters")
     private String id;
 
     @NotNull(message = "From Currency ISO Code must not be null")
     @NotEmpty(message = "From Currency ISO Code must not be empty")
+    @Size(max = 3, min = 3, message = "From Currency ISO Code should be 3 characters. Example: `EUR`")
     private String fromCurrencyISOCode;
 
     @NotNull(message = "To Currency ISO Code must not be null")
     @NotEmpty(message = "To Currency ISO Code must not be empty")
+    @Size(max = 3, min = 3, message = "To Currency ISO Code should be 3 characters. Example: `GBP`")
     private String toCurrencyISOCode;
 
     private LocalDateTime dealTimestamp;
